c87e889833baaa832c67b1ef406565d2bb591ca9
jdc183 <54681412+jdc183@users.noreply.github.com>
2020-07-15 12:03:22 -0400

    Add hbridge control to tester.ino

--- tester/tester.ino
+++ tester/tester.ino
@@ -15,6 +15,16 @@ void setup() {
   pinMode(13,OUTPUT);
   pinMode(ADC_PIN,OUTPUT);
   pinMode(DAC_PIN,OUTPUT);
+  pinMode(H_EN,OUTPUT);
+  pinMode(H_POS,OUTPUT);
+  pinMode(H_NEG,OUTPUT);
+
+  pinMode(SC1_PIN,INPUT);
+  pinMode(SC2_PIN,INPUT);
+  pinMode(SC3_PIN,INPUT);
+  
+  digitalWrite(H_POS,HIGH);
+  digitalWrite(H_NEG,LOW);
 }
 
 void loop() {
@@ -25,6 +35,12 @@ void loop() {
 
   rx = Serial.readStringUntil('\n');//get recieved data
   received = rx.toFloat();  //convert it to a float
+  if(received > 0.001){
+    digitalWrite(H_EN,HIGH);
+  }
+  else{
+    digitalWrite(H_EN,LOW);
+  }
   out = received / 3.3 * 4095.0; //16bit int to write to dac
   DAC_write(out);  //Write this to the DAC
   sense = ADC_read();  //Read the value from the ADC
@@ -38,7 +54,7 @@ void loop() {
 /* SPI functions */
 /* ************************ */
 
-/*  read a 12-bit value from the MCP3201 ADC on a specified channel (0 through NCHANNELS-1) */
+/*  read a 12-bit value from the MCP3201 ADC  */
 uint16_t ADC_read() {
     digitalWrite(ADC_PIN, LOW);
     uint16_t ret1(SPI.transfer16(0x0000)); 
@@ -46,7 +62,7 @@ uint16_t ADC_read() {
     return ret1;
 }
 
-/*  write a 12-bit value to the MCP4921 DAC on a specified channel (0 through NCHANNELS-1) */
+/*  write a 12-bit value to the MCP4921 DAC  */
 void DAC_write(uint16_t to_dac) {       
     byte dataMSB = highByte(to_dac);
     byte dataLSB = lowByte(to_dac);
